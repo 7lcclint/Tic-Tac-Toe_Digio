c8ac934e6c3ac65720db1339592423348b8c338c
Pakorn <pakorn.clint@outlook.com>
2024-09-05 14:46:21 +0700

    Update database.js

--- packages/backend/database.js
+++ packages/backend/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'seraveeII1/100',
-  database: 'tictactoe'
+  host: '',
+  user: '',
+  password: '',
+  database: ''
 });
 
 connection.connect(error => {
